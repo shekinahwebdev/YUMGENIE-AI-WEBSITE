3f43c45a0e57fc54659043320952f239c83467ed
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-08-21 23:30:01 +0000

    add selection highlight and toggle add-to-cart functionality in Food component

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -1,5 +1,6 @@
 import addToCart from "/assets/icons/icon-add-cart.svg";
 import "./Food.css";
+import { useState } from "react";
 
 interface FoodProps {
   food: {
@@ -11,54 +12,67 @@ interface FoodProps {
 }
 
 const Food: React.FC<FoodProps> = ({ food }) => {
+  const [isAddToCart, setIsAddToCart] = useState(true);
+
+  const handleAddToCart = () => {
+    setIsAddToCart((prev) => !prev);
+  };
+
   return (
     <section className="food-section">
       {food.map((list, index) => (
         <div className="foodName" key={index}>
-          <img src={list.src} alt="fruts" className="foodImage" />
+          <img
+            src={list.src}
+            alt="fruts"
+            className={`foodImage ${!isAddToCart ? "selected" : ""}`}
+          />
           <div className="food-item-add">
-            <button className="add-section btn">
-              <img
-                src={addToCart}
-                alt="Added to Cart"
-                className="food-item__add-icon"
-              />
-              <p className="add_cart">Add to Cart</p>
-            </button>
-          </div>
-          <div className="quantity-controls">
-            <button
-              className="food-item__button decrement"
-              // onClick={handleDecrement}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="10"
-                height="2"
-                fill="none"
-                viewBox="0 0 10 2"
-              >
-                <path fill="currentColor" d="M0 .375h10v1.25H0V.375Z" />
-              </svg>
-            </button>
-            <p>0</p>
-            <button
-              className="food-item__button increment"
-              // onClick={handleIncrement}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="10"
-                height="10"
-                fill="none"
-                viewBox="0 0 10 10"
-              >
-                <path
-                  fill="currentColor"
-                  d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+            {isAddToCart ? (
+              <button className="add-section btn" onClick={handleAddToCart}>
+                <img
+                  src={addToCart}
+                  alt="Added to Cart"
+                  className="food-item__add-icon"
                 />
-              </svg>
-            </button>
+                <p className="add_cart">Add to Cart</p>
+              </button>
+            ) : (
+              <div className="quantity-controls">
+                <button
+                  className="food-item__button decrement"
+                  // onClick={handleDecrement}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    height="2"
+                    fill="none"
+                    viewBox="0 0 10 2"
+                  >
+                    <path fill="currentColor" d="M0 .375h10v1.25H0V.375Z" />
+                  </svg>
+                </button>
+                <p>0</p>
+                <button
+                  className="food-item__button increment"
+                  // onClick={handleIncrement}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="10"
+                    height="10"
+                    fill="none"
+                    viewBox="0 0 10 10"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            )}
           </div>
           <div className="item_class">
             <span className="food-item__category">{list.category}</span>
